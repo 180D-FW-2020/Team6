dd598a64fe1e0b2e393c1e688f5ac58f497e5343
kenryhou2 <kouhenry@yahoo.com>
2020-12-02 17:07:34 -0800

    in progress streaming client as opencv

--- src/video_processing/stream_client/stream_client_motion.py
+++ src/video_processing/stream_client/stream_client_motion.py
@@ -30,6 +30,21 @@ try:
     # our protocol simple)
     start = time.time()
     stream = io.BytesIO()
+    with picamera.PiCamera() as camera:
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(stream, format='jpeg')
+    # Construct a numpy array from the stream
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    # "Decode" the image from the array, preserving colour
+    image = cv2.imdecode(data, 1)
+    # OpenCV returns an array with data in BGR order. If you want RGB instead
+    # use the following...
+    image = image[:, :, ::-1]
+
+
+
+    
     for foo in camera.capture_continuous(stream, 'jpeg'):
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
