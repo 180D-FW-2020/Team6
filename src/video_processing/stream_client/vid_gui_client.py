ad2f684d23f58b60b80d51e065f69dfea75d1cea
kenryhou2 <kouhenry@yahoo.com>
2020-12-07 19:20:43 -0800

    update server and client

--- src/video_processing/stream_client/vid_gui_client.py
+++ src/video_processing/stream_client/vid_gui_client.py
@@ -4,20 +4,38 @@ import struct
 from PIL import Image
 import matplotlib.pyplot as pl
 
-server_socket = socket.socket()
-server_socket.bind(('3.15.16.101', 8001))  # ADD IP HERE
-
-server_socket.listen(0)
+gui_client_socket = socket.socket()
+gui_client_socket.connect(('3.15.16.101', 6667))  # ADD IP HERE
 
 # Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
+#connection = gui_client_socket.makefile('rb')
+try:
+    message = "message from gui_laptop"
+    while True:
+        print("sending: " + message)
+        gui_client_socket.sendall(message)
+        
+
+finally:
+    print("closing socket")
+    gui_client_socket.close()
+
+
+
 try:
     img = None
     while True:
+        print("running")
         # Read the length of the image as a 32-bit unsigned int. If the
         # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+
+        connection.read(struct.calcsize('<L'))
+        print("reading")
+
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0] #stuck here
+        print("unpacked")
         if not image_len:
+            print("not image len")
             break
         # Construct a stream to hold the image data and read the image
         # data from the connection
@@ -29,8 +47,11 @@ try:
         image = Image.open(image_stream)
         
         if img is None:
+            print("Image is none")
             img = pl.imshow(image)
+            
         else:
+            print("image is set")
             img.set_data(image)
 
         pl.pause(0.0001)
@@ -41,4 +62,4 @@ try:
         #print('Image is verified')
 finally:
     connection.close()
-    server_socket.close()
+    gui_client_socket.close()
---
+++ src/video_processing/stream_server/stream_multi_thread_server.py
@@ -0,0 +1,44 @@
+#!usr/bin/python
+import io
+from PIL import Image
+import matplotlib.pyplot as pl
+from thread import *
+import socket
+import sys
+
+def clientthread(conn):
+    buffer=""
+    while True:
+        data = conn.recv(8192)
+        buffer+=data
+        print buffer
+    #conn.sendall(reply)
+    conn.close()
+
+def main():
+    try:
+        host = '0.0.0.0'
+        port = 6666
+        tot_socket = 2
+        list_sock = []
+        for i in range(tot_socket):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+            s.bind((host, port+i))
+            s.listen(10)
+            list_sock.append(s)
+            print "[*] Server listening on %s %d" %(host, (port+i))
+
+        while 1:
+            for j in range(len(list_sock)):
+                conn, addr = list_sock[j].accept()
+                print '[*] Connected with ' + addr[0] + ':' + str(addr[1])
+                start_new_thread(clientthread ,(conn,))
+        s.close()
+
+    except KeyboardInterrupt as msg:
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
