a01c1de490753cc8e6a96d1345e557fb03909f33
kenryhou2 <kouhenry@yahoo.com>
2020-12-07 17:14:23 -0800

    update central_processing video server

---
+++ src/video_processing/stream_server/stream_central_process_2.py
@@ -0,0 +1,59 @@
+import io
+import socket
+import struct
+from PIL import Image
+import matplotlib.pyplot as pl
+
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))  #server socket for receiving from RPI and CAMERA
+server_socket.listen(0)
+
+gui_socket = socket.socket()
+gui_socket.bind(('0.0.0.0',8001)) #server socket for transmitting to GUI
+gui_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+connection_gui = gui_socket.accept()[0].makefile('wb') #TODO WB?RB?
+try:
+    img = None
+    while True:
+        print("running")
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+        
+        # if img is None:
+        #     img = pl.imshow(image)
+        # else:
+        #     img.set_data(image)
+
+        # pl.pause(0.0001)
+        # pl.draw()
+
+        #Sending to gui client
+        connection_gui.write(struct.pack('<L', image.tell()))
+        image.seek(0)
+        image.truncate()
+
+
+        #print('Image is %dx%d' % image.size)
+        #image.verify()
+        #print('Image is verified')
+finally:
+    connection_gui.write(struct.pack('<L', 0))
+    print("closing")
+    connection_gui.close()
+    connection.close()
+    server_socket.close()
+    gui_socket.close()
