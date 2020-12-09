096597152fc574d5821c197740998456499360a8
kenryhou2 <kouhenry@yahoo.com>
2020-12-09 10:31:50 -0800

    update central process server and gui client

--- src/video_processing/stream_client/vid_gui_client.py
+++ src/video_processing/stream_client/vid_gui_client.py
@@ -7,59 +7,56 @@ import matplotlib.pyplot as pl
 gui_client_socket = socket.socket()
 gui_client_socket.connect(('3.15.16.101', 6667))  # ADD IP HERE
 
-# Accept a single connection and make a file-like object out of it
-#connection = gui_client_socket.makefile('rb')
-try:
-    while True:
-
-        data = gui_client_socket.recv(8192)
-        print(data)
-        
-
-finally:
-    print("closing socket")
-    gui_client_socket.close()
-
-
 
 # try:
-#     img = None
 #     while True:
-#         print("running")
-#         # Read the length of the image as a 32-bit unsigned int. If the
-#         # length is zero, quit the loop
 
-#         connection.read(struct.calcsize('<L'))
-#         print("reading")
-
-#         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0] #stuck here
-#         print("unpacked")
-#         if not image_len:
-#             print("not image len")
-#             break
-#         # Construct a stream to hold the image data and read the image
-#         # data from the connection
-#         image_stream = io.BytesIO()
-#         image_stream.write(connection.read(image_len))
-#         # Rewind the stream, open it as an image with PIL and do some
-#         # processing on it
-#         image_stream.seek(0)
-#         image = Image.open(image_stream)
+#         data = gui_client_socket.recv(8192)
+#         print(data)
         
-#         if img is None:
-#             print("Image is none")
-#             img = pl.imshow(image)
-            
-#         else:
-#             print("image is set")
-#             img.set_data(image)
 
-#         pl.pause(0.0001)
-#         pl.draw()
-
-#         #print('Image is %dx%d' % image.size)
-#         #image.verify()
-#         #print('Image is verified')
 # finally:
-#     connection.close()
+#     print("closing socket")
 #     gui_client_socket.close()
+
+
+
+try:
+    while True:
+        print("running")
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+
+        connection.read(struct.calcsize('<L'))
+        print("reading")
+
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0] #stuck here
+        print("unpacked")
+        if not image_len:
+            print("not image len")
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+        
+        if img is None:
+            print("Image is none")
+            img = pl.imshow(image)
+            
+        else:
+            print("image is set")
+            img.set_data(image)
+
+        pl.pause(0.0001)
+        pl.draw()
+
+        #print('Image is %dx%d' % image.size)
+        #image.verify()
+        #print('Image is verified')
+finally:
+    gui_client_socket.close()
---
+++ src/video_processing/stream_server/stream_central_process_1.py
@@ -0,0 +1,59 @@
+#!usr/bin/python
+import io
+from PIL import Image
+import struct
+import matplotlib.pyplot as pl
+import socket
+import sys
+
+def main():
+    try:
+        host = '0.0.0.0'
+        port = 6666
+        tot_socket = 2
+        rpi_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        rpi_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        rpi_sock.bind((host, port))
+        rpi_sock.listen(10)
+        print("[*] Server listening on port")
+        rpi_client_conn = rpi_sock.accept()[0].makefile('rb')
+        print("Rpi connected")
+
+        gui_sock = socket.socket()
+        gui_sock.connect(('192.168.1.239',6667))#connects to gui laptop IP
+        print("[*] Client listening on port")
+        gui_conn = gui_sock.makefile('wb')
+        print("Gui connected")
+
+        while True:    
+            print("running")
+            image_len = struct.unpack('<L', rpi_client_conn.read(struct.calcsize('<L')))[0] #unpacks from buffer of bytes
+            image_len is a bytes like object to be written in to the image stream 
+            if not image_len:
+                print("no image stream was unpacked")
+                break
+            image_stream = io.BytesIO() #stream of bytes
+            image_stream.write(rpi_client_conn.read(image_len))  #writes the byte like object from rpi to the raw stream
+            
+            #image = Image.open(image_stream) #open the PIL image
+
+
+            gui_conn.write(struct.pack('<L',image_stream.tell())) #reports the size of the entire stream at current point.. end of stream?
+            gui_conn.flush() #flush content to a file...
+            image_stream.seek(0) #change stream position to start of stream, 0
+            gui_conn.write(image_stream.read())
+            image_stream.seek(0)
+            image_stream.truncate()
+        gui_conn.write(struct.pack('<L',0))
+        
+    finally:
+        gui_conn.close()
+        gui_sock.close()
+        rpi_client_conn.close()
+        rpi_sock.close()
+    except KeyboardInterrupt as msg:
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
