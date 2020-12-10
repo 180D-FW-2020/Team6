423e15e7cb1f5d2dfe86736f3ae3592757e62e4b
kenryhou2 <kouhenry@yahoo.com>
2020-12-09 22:06:23 -0800

    base code for streaming video from central processing

--- src/video_processing/stream_client/vid_gui_client.py
+++ src/video_processing/stream_client/vid_gui_client.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as pl
 #gui_client_socket.connect(('0.0.0.0', 6667))  # ADD IP HERE
 gui_sock = socket.socket()
 gui_sock.bind(('0.0.0.0',6667))
-gui_sock.listen(10)
+gui_sock.listen(0)
 connection = gui_sock.accept()[0].makefile('rb')
 
 # try:
@@ -16,57 +16,46 @@ connection = gui_sock.accept()[0].makefile('rb')
 
 #         data = gui_client_socket.recv(8192)
 #         print(data)
-        
+
 
 # finally:
 #     print("closing socket")
 #     gui_client_socket.close()
 
-
-
 try:
-    while True:
-        print("running")
-        # Read the length of the image as a 32-bit unsigned int. If the
-        # length is zero, quit the loop
+	img = None
+	while True:
+		print("running")
+		image_len = struct.unpack('<L',connection.read(struct.calcsize('<L')))[0]
+		if not image_len:
+			print("not image len")
+			break
+			#continue
+		print(image_len)
+		# Construct a stream to hold the image data and read the image
+		# data from the connection
+		image_stream = io.BytesIO()
+		#print(connection.read(image_len))
+		image_stream.write(connection.read(image_len)) #stuck here
+		print("stream_written")
+		# Rewind the stream, open it as an image with PIL and do some
+		# processing on it
+		image_stream.seek(0)
+		image = Image.open(image_stream)
+		print("image opened")
 
-        connection.read(struct.calcsize('<L'))
-        print("reading")
+		if img is None:
+			print("Image is none")
+			img = pl.imshow(image)
+		else:
+			print("image is set")
+			img.set_data(image)
 
+		pl.pause(0.0001)
+		pl.draw()
 
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0] #stuck here
-        print("unpacked")
-        if not image_len:
-            print("not image len")
-            #break
-            continue
-        print(image_len)
-
-        break
-        # # Construct a stream to hold the image data and read the image
-        # # data from the connection
-        # image_stream = io.BytesIO()
-        # image_stream.write(connection.read(image_len))
-        # print("stream_written")
-        # # Rewind the stream, open it as an image with PIL and do some
-        # # processing on it
-        # image_stream.seek(0)
-        # image = Image.open(image_stream)
-        # print("image opened")
-        
-        # if img is None:
-        #     print("Image is none")
-        #     img = pl.imshow(image)
-            
-        # else:
-        #     print("image is set")
-        #     img.set_data(image)
-
-        # pl.pause(0.0001)
-        # pl.draw()
-
-        # print('Image is %dx%d' % image.size)
-        # image.verify()
-        # print('Image is verified')
+		# print('Image is %dx%d' % image.size)
+		# image.verify()
+		# print('Image is verified')
 finally:
-    gui_client_socket.close()
+	gui_client_socket.close()
