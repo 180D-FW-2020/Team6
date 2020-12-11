5ec4855c586b151a3c14c1733c4280cde69f1161
kenryhou2 <kouhenry@yahoo.com>
2020-12-10 18:52:01 -0800

    update video client

--- src/video_processing/stream_client/vid_gui_client.py
+++ src/video_processing/stream_client/vid_gui_client.py
@@ -16,8 +16,8 @@ from email.message import EmailMessage
 #gui_client_socket = socket.socket()
 #gui_client_socket.connect(('0.0.0.0', 6667))  # ADD IP HERE
 
-_email = "" # sender email addr
-_pass =  "" # sender email password
+_email = "nightlight.notifier@gmail.com" # sender email addr
+_pass =  "qjhlwonnufdgvdss" # sender email password
 
 def notify(subject, content, to):
     print("Pushing notification")
@@ -35,6 +35,8 @@ def notify(subject, content, to):
 
 motion_reset_time = 5.0 #seconds
 minArea = 1000 #area of detection for motion detection trigger
+
+motion_detect = False
 
 gui_sock = socket.socket()
 gui_sock.bind(('0.0.0.0',6667))
@@ -77,6 +79,8 @@ try:
 		# processing on it
 		image_stream.seek(0)
 
+
+
 		#motion detection
 		file_bytes = np.asarray(bytearray(image_stream.read()),dtype=np.uint8)
 		raw_frame = cv2.imdecode(file_bytes,cv2.IMREAD_COLOR)
@@ -84,70 +88,72 @@ try:
 			break
 		text = "None Detected"
 
-
-
-		#drawing crib...
-		height, width = raw_frame.shape[0], raw_frame.shape[1]
-		target_upper_left = (((width//2)-200),((height//2)-200)) #setting up coords for target area
-		target_lower_right = (((width//2)+200),((height//2)+200))
-		target_box = cv2.rectangle(raw_frame,target_upper_left,target_lower_right,(255,0,0),3)
-		target_area = raw_frame[target_upper_left[1]:target_lower_right[1], target_upper_left[0]:target_lower_right[0]] #y1:y2,x1:x2 where y is lower_right, and x is upper left
-
-		# resize the target area, convert it to grayscale, and blur it
-		frame = imutils.resize(target_area, width=500)
-		gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-		# if the first frame is None, initialize it
-		#grab firstFrame time
-		
-		firstFrame_end = time.time()
-		if firstFrame_end - firstFrame_start > motion_reset_time:
-			firstFrame_start = firstFrame_end
-			#print("resetting image")
-			firstFrame  = None
-		if firstFrame is None:
-			firstFrame = gray
-			continue
-
-		# compute the absolute difference between the current frame and
-		# first frame
-		frameDelta = cv2.absdiff(firstFrame, gray)
-		thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-
-		# dilate the thresholded image to fill in holes, then find contours
-		# on thresholded image
-		thresh = cv2.dilate(thresh, None, iterations=2)
-		thresh = thresh.astype(np.uint8)
-		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		cnts = imutils.grab_contours(cnts)
-
-		# loop over the contours
-		notif_flag = False
-		for c in cnts:
-
-			# if the contour is too small, ignore it
-			if cv2.contourArea(c) < minArea:
-				continue
-		    # compute the bounding box for the contour, draw it on the frame,
-			# and update the text
-			(x, y, w, h) = cv2.boundingRect(c)
-			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-			text = "Detected!"
-			if notif_flag == False
-				#for some reason this sends twice
-				notify("Motion Detected from NightLight","Some movement was caught on the NightLight, check things out to make sure everything's okay :) - Your Nightlight","kouhenry@yahoo.com")
-				notif_flag = True
-
-		# draw the text and timestamp on the frame
-		cv2.putText(raw_frame, "Motion Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-		cv2.putText(raw_frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-		# show the frame and record if the user presses a key
 		cv2.imshow("Camera feed", raw_frame)
-		cv2.imshow("Thresh", thresh)
-		#cv2.imshow("Frame Delta", frameDelta)
-		cv2.imshow('target area', target_area)
+
+
+		if motion_detect is True:
+			#drawing crib...
+			height, width = raw_frame.shape[0], raw_frame.shape[1]
+			target_upper_left = (((width//2)-200),((height//2)-200)) #setting up coords for target area
+			target_lower_right = (((width//2)+200),((height//2)+200))
+			target_box = cv2.rectangle(raw_frame,target_upper_left,target_lower_right,(255,0,0),3)
+			target_area = raw_frame[target_upper_left[1]:target_lower_right[1], target_upper_left[0]:target_lower_right[0]] #y1:y2,x1:x2 where y is lower_right, and x is upper left
+
+			# resize the target area, convert it to grayscale, and blur it
+			frame = imutils.resize(target_area, width=500)
+			gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY)
+			gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+			# if the first frame is None, initialize it
+			#grab firstFrame time
+			
+			firstFrame_end = time.time()
+			if firstFrame_end - firstFrame_start > motion_reset_time:
+				firstFrame_start = firstFrame_end
+				#print("resetting image")
+				firstFrame  = None
+			if firstFrame is None:
+				firstFrame = gray
+				continue
+
+			# compute the absolute difference between the current frame and
+			# first frame
+			frameDelta = cv2.absdiff(firstFrame, gray)
+			thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+			# dilate the thresholded image to fill in holes, then find contours
+			# on thresholded image
+			thresh = cv2.dilate(thresh, None, iterations=2)
+			thresh = thresh.astype(np.uint8)
+			cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+			cnts = imutils.grab_contours(cnts)
+
+			# loop over the contours
+			notif_flag = False
+			for c in cnts:
+
+				# if the contour is too small, ignore it
+				if cv2.contourArea(c) < minArea:
+					continue
+			    # compute the bounding box for the contour, draw it on the frame,
+				# and update the text
+				(x, y, w, h) = cv2.boundingRect(c)
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+				text = "Detected!"
+				if notif_flag is False:
+				#for some reason this sends twice
+					notify("Motion Detected from NightLight","Some movement was caught on the NightLight, check things out to make sure everything's okay :) - Your Nightlight","kouhenry@yahoo.com")
+					notif_flag = True
+
+			# draw the text and timestamp on the frame
+			cv2.putText(raw_frame, "Motion Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+			cv2.putText(raw_frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+			# show the frame and record if the user presses a key
+			
+			cv2.imshow("Thresh", thresh)
+			#cv2.imshow("Frame Delta", frameDelta)
+			cv2.imshow('target area', target_area)
 		key = cv2.waitKey(1) & 0xFF
 
 		# if the `q` key is pressed, break from the lop
