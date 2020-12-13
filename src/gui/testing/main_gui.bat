cb816eb154bc4af5cdc294276bf9b2e1d6e017bf
kenryhou2 <kouhenry@yahoo.com>
2020-12-12 21:35:50 -0800

    main_gui.bat: now can run all client files

--- src/gui/testing/main_gui.bat
+++ src/gui/testing/main_gui.bat
@@ -1,5 +1,5 @@
 @echo off
+ECHO starting main_gui.py
+START python main_gui.py
 ECHO starting vid_gui_client.py
-START "" main_gui.py
-"C:\Users\Henry Kou\miniconda3\envs\hkou_env_tutorial\python.exe" "C:\Users\Henry Kou\Desktop\EE180DA\nightlight_project\Team6\src\gui\testing\vid_gui_client.py"
-ECHO "C:\Users\Henry Kou\miniconda3\envs\hkou_env_tutorial\python.exe" "C:\Users\Henry Kou\Desktop\EE180DA\nightlight_project\Team6\src\gui\testing\main_gui.py"
+START python ..\..\video_processing\stream_client\vid_gui_client.py
