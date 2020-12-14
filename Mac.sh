d12838568b5b8ad1fd9a66c18e378d076e8bcf54
Denny Tsai <denny880320@ucla.edu>
2020-12-14 15:35:33 +0800

    script runs two

--- Mac.sh
+++ Mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd src/video_processing/stream_client
-python3 vid_gui_client.py
+open -a Terminal.app script1.command & open -a Terminal.app script2.command
+
+
 
---
+++ script1.command
@@ -0,0 +1,2 @@
+cd Desktop/Team6/src/video_processing/stream_client 
+python3 vid_gui_client.py
