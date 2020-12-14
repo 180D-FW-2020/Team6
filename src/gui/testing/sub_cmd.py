4c38784e1388a6ca56e5fc15fcf4bb96bb2aec6b
Leondi <leondi2000@gmail.com>
2020-12-13 19:48:23 -0800

    Solving the subscriber code for GUI

--- src/gui/testing/sub_cmd.py
+++ src/gui/testing/sub_cmd.py
@@ -4,52 +4,56 @@
 import random
 from paho.mqtt import client as mqtt_client
 
-
-class sub_cmd:
-    def __init__(self, port, topic):
-        # The address of the subscriber to get the message from the publisher
-        self.broker = 'broker.emqx.io'
-        self.port = 1883
-        self.topic = "/python/mqtt/"
-        # generate client ID with pub prefix randomly
-        self.client_id = f'python-mqtt-{random.randint(0, 100)}'
+# The address of the subscriber to get the message from the publisher
+broker = 'broker.emqx.io'
+port = 1883
+topic = "/python/mqtt/"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+message = None
 
 
-    def connect_mqtt(self) -> mqtt_client:
-    # 0. define callbacks - functions that run when events happen.
-    # The callback for when the client receives a CONNACK response from the server.
-    # Not 0, the callback of the client when it disconnects.
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                print("Connected to MQTT Broker!")
-            else:
-                print("Failed to connect, return code %d\n", rc)
-        
-        # Add callbacks to client.
-        # Set Connecting Client ID
-        self.client = mqtt_client.Client(self.client_id)
-        self.client.on_connect = on_connect
-        self.client.connect(self.broker, self.port)
-        return client
+def connect_mqtt() -> mqtt_client:
+# 0. define callbacks - functions that run when events happen.
+# The callback for when the client receives a CONNACK response from the server.
+# Not 0, the callback of the client when it disconnects.
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    # Add callbacks to client.
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 
-    def subscribe(self, client: mqtt_client):
-    # The default message callback.
-    # (you can create separate callbacks per subscribed topic)
-        def on_message(client, userdata, msg):
-            print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+def subscribe(client: mqtt_client):
+# The default message callback.
+# (you can create separate callbacks per subscribed topic)
+    def on_message(client, userdata, msg):
+        global message 
+        message = msg.payload.decode()
+        print(f"Received `{message}` from `{msg.topic}` topic")
 
-        self.client.subscribe(self.topic)
-        self.client.on_message = on_message
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+def get_mes():
+    return message
 
 
-    def run(self):
-        self.client = self.connect_mqtt()
-        self.subscribe(self.client)
-        self.client.loop_forever()
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    print (get_mes())
+    client.loop_forever()
 
 
 if __name__ == '__main__':
-    s = sub_cmd()
-    s.run()
+    run()
 
