85ce0e30de9603f31275b5805f0e8f37d6c5fa2b
robertrenzorudio <68797863+robertrenzorudio@users.noreply.github.com>
2020-12-15 00:12:27 -0800

    Update AudioClient.py

--- src/comms/AudioClient.py
+++ src/comms/AudioClient.py
@@ -12,12 +12,13 @@ sample_size = pyaudio.get_sample_size(FORMAT)
 SAVE_PATH = "../../recordings/audio/"
 
 class AudioClient:
-    def __init__(self, ip="3.15.16.101", port=3001):
+    def __init__(self, ip="3.15.16.101", port=3001, write=False):
         self.ip = ip 
         self.port = port
         self.client_conn= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.p = None
         self.stream = None
+        self.write = write
     
     def start(self):
         self.client_conn.connect((self.ip, self.port))
@@ -30,26 +31,26 @@ class AudioClient:
                                   input=True,
                                   output=True,
                                   frames_per_buffer=CHUNK)
-    
+
     def stop(self):
         self.client_conn.shutdown(socket.SHUT_RDWR)
         self.client_conn.close()
 
     def recv(self):
         #frames = []
-        #temp = None
-        data = self.client_conn.recv(BUFFERSIZE)
-
-        #while len(data) < BUFFERSIZE:
-       #     temp = self.client_conn.recv(BUFFERSIZE - len(data))
-       #     data += temp
+        while True:
+            data = self.client_conn.recv(BUFFERSIZE)
         
-        """
-        if len(data) <= 4 and len(data) != 0:
-            self.save_wav(frames)
-            return
-        """
-        self.stream.write(data)
+            while len(data) < BUFFERSIZE:
+                temp = self.client_conn.recv(BUFFERSIZE - len(data))
+                data += temp
+            """    
+            if len(data) <= 4 and len(data) != 0:
+                self.save_wav(frames)
+                return
+            """
+            if self.write:
+                self.stream.write(data)
 
         #frames.append(data)
 
