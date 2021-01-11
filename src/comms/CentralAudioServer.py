d0159f5fa9c3dddf2908952d7bc663416a93d596
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-01-10 18:51:01 -0800

    Added multiple client connection

--- src/comms/CentralAudioServer.py
+++ src/comms/CentralAudioServer.py
@@ -1,20 +1,93 @@
+#!/usr/bin/env python
+
 import io
 import socket
 import threading
 import wave
 from datetime import datetime
 
-BUFFERSIZE = 2048
 SIGLEN = 6
 RATE = 16000
 CHUNK = 1024
+BUFFERSIZE = CHUNK
 CHANNELS = 1
 SAMPLESIZE = 2
 SEC = RATE/CHUNK
 MAX_REC_SEC = 60
 SAVEPATH = "recordings/audio/"
 
+AUDIO = None
+
+def redirect(CLIENT):
+    global AUDIO
+
+    while not AUDIO:
+        pass
+    
+    while True:
+        data = AUDIO.recv(BUFFERSIZE)
+        CLIENT.send(data)
+
+
+def client_connection(): 
+    listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_sock.bind(('0.0.0.0', 4000))  # socket for listening
+    listen_sock.listen(5)
+
+    while True:
+        cli, addr = listen_sock.accept()
+        redirect_thread = threading.Thread(target=redirect, args=(cli,))
+        redirect_thread.start()
+        print("Accepted connection from client")
+
+def audio_connection():
+    global AUDIO 
+    
+    audio_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    audio_client.bind(('0.0.0.0', 4001))  # socket for RPI Microphone
+    audio_client.listen(0)
+    AUDIO, addr = audio_client.accept()
+    print("Accepted connection fro RPI Microphone")
+
+def main():
+    client_listner_thread = threading.Thread(target=client_connection)
+    client_listner_thread.start()
+
+    audio_listner_thread = threading.Thread(target=audio_connection)
+    audio_listner_thread.start()
+        
+if __name__== "__main__":
+    print("Audio Server Is Running")
+    main()
+
+"""
+def rms(frame):
+    count = len(frame) / swidth
+    format = "%dh" % (count)
+    shorts = struct.unpack(format, frame)
+    sum_squares = 0.0
+    for sample in shorts:
+        n = sample * SHORT_NORMALIZE
+        sum_squares += n * n
+    rms = (sum_squares / count) ** 0.5
+    return rms * 1000
+
+def save_wav(frames):
+    print("Saving...")
+    now = datetime.now()
+    now_str = now.strftime("%d-%m-%Y-%H:%M:%S.wav")
+
+    wf = wave.open(SAVEPATH + now_str, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(SAMPLESIZE)
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    print("...Saved")
+
 def record(audio_conn, gui_conn):
+
     print("Recording Audio ...")
 
     max_iter = int(SEC * MAX_REC_SEC)
@@ -35,57 +108,4 @@ def record(audio_conn, gui_conn):
 
     th = threading.Thread(target=save_wav, args=(frames,))
     th.start()
-
-def save_wav(frames):
-    print("Saving...")
-    now = datetime.now()
-    now_str = now.strftime("%d-%m-%Y-%H:%M:%S.wav")
-
-    wf = wave.open(SAVEPATH + now_str, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(SAMPLESIZE)
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    print("...Saved")
-
-def main():
-    audio_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    audio_client.bind(('0.0.0.0', 2000))  # socket for RPI Microphone
-    audio_client.listen(0)
-    audio_conn, addr = audio_client.accept()
-    print("Accepted connection from Microphone")
-
-    gui_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    gui_client.bind(('0.0.0.0', 2001))  # socket for RPI Microphone
-    gui_client.listen(0)
-    gui_conn, addr = gui_client.accept()
-    print("Accepted connection from GUI")
-
-    while True:
-        data = audio_conn.recv(BUFFERSIZE)        
-        
-        temp = None
-        while len(data) != BUFFERSIZE:
-            temp = audio_conn.recv(BUFFERSIZE - len(data))
-            data += temp
-            
-        gui_conn.send(data)
-        
-        """
-        if len(data) <= SIGLEN and len(data) != 0:
-            print("HERE")
-            record(audio_conn, gui_conn)
-            continue
-        if len(data) <= 4 and len(data) != 0:
-            gui_conn.send(data)
-            continue
-
-        while len(data) < BUFFERSIZE:
-            temp = audio_conn.recv(BUFFERSIZE)
-            data += temp
-        """
-
-if __name__=="__main__":
-    main()
+"""
