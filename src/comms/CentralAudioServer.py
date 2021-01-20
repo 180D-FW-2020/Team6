c9099b614c9364797c9aaac2188d2b3048791d82
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-01-19 23:52:36 -0800

    CentralAudioServer.py:     - Updated data redirection to mutliple client     - Clients can now exit program without crashing other clients.

--- src/comms/CentralAudioServer.py
+++ src/comms/CentralAudioServer.py
@@ -16,28 +16,49 @@ SEC = RATE/CHUNK
 MAX_REC_SEC = 60
 SAVEPATH = "recordings/audio/"
 
-AUDIO = None
+mutex = threading.Lock()
 
-def redirect(CLIENT):
-    global AUDIO
+AUDIO = None
+CLIENTS = []
+
+def redirect():
+    global AUDIO, CLIENTS
 
     while not AUDIO:
         pass
     
     while True:
+        closed = []
         data = AUDIO.recv(BUFFERSIZE)
-        CLIENT.send(data)
+    
+        mutex.acquire()
+        for client in CLIENTS:
+            try:
+                client.send(data)
+            except:
+                closed.append(client)
 
 
-def client_connection(): 
+        print(closed)
+        print(CLIENTS)
+        # Drop closed connections       
+        for drop in closed:
+            CLIENTS.remove(drop)
+
+        mutex.release()
+
+def client_connection():
+    global CLIENTS
     listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_sock.bind(('0.0.0.0', 4000))  # socket for listening
     listen_sock.listen(5)
 
     while True:
-        cli, addr = listen_sock.accept()
-        redirect_thread = threading.Thread(target=redirect, args=(cli,))
-        redirect_thread.start()
+        cli, _ = listen_sock.accept()
+        mutex.acquire()
+        CLIENTS.append(cli)
+        mutex.release()
+
         print("Accepted connection from client")
 
 def audio_connection():
@@ -46,7 +67,7 @@ def audio_connection():
     audio_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     audio_client.bind(('0.0.0.0', 4001))  # socket for RPI Microphone
     audio_client.listen(0)
-    AUDIO, addr = audio_client.accept()
+    AUDIO, _ = audio_client.accept()
     print("Accepted connection fro RPI Microphone")
 
 def main():
@@ -55,57 +76,10 @@ def main():
 
     audio_listner_thread = threading.Thread(target=audio_connection)
     audio_listner_thread.start()
+
+    redirect_thread = threading.Thread(target=redirect)
+    redirect_thread.start()
         
 if __name__== "__main__":
     print("Audio Server Is Running")
     main()
-
-"""
-def rms(frame):
-    count = len(frame) / swidth
-    format = "%dh" % (count)
-    shorts = struct.unpack(format, frame)
-    sum_squares = 0.0
-    for sample in shorts:
-        n = sample * SHORT_NORMALIZE
-        sum_squares += n * n
-    rms = (sum_squares / count) ** 0.5
-    return rms * 1000
-
-def save_wav(frames):
-    print("Saving...")
-    now = datetime.now()
-    now_str = now.strftime("%d-%m-%Y-%H:%M:%S.wav")
-
-    wf = wave.open(SAVEPATH + now_str, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(SAMPLESIZE)
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-    print("...Saved")
-
-def record(audio_conn, gui_conn):
-
-    print("Recording Audio ...")
-
-    max_iter = int(SEC * MAX_REC_SEC)
-    frames = []
-    temp = None
-    for i in range(max_iter):
-        data = audio_conn.recv(BUFFERSIZE)
-        datalen = len(data)
-
-        while datalen != 2048:
-            temp = audio_conn.recv(BUFFERSIZE-datalen) 
-            data += temp
-
-        gui_conn.send(data)
-        frames.append(data)
-
-    print("...Done Recording")
-
-    th = threading.Thread(target=save_wav, args=(frames,))
-    th.start()
-"""
