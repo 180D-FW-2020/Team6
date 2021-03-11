3c30078bd6f1cbb4adc4077e4e426db553f2ddf8
Denny Tsai <denny880320@ucla.edu>
2021-03-11 14:41:57 +0800

    shell script

--- Mac.sh
+++ Mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-TPATH=$(find . -type f -name "GUI.py")
-python3 $TPATH
+TPATH=$(find . -type f -name "GUIMAC.py")
+BPATH=$(find . -type f -name "sub_cmd.py")
+python3 $TPATH & python3 $BPATH
 
 
 
